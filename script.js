455c33dd9d6f0b83cdf57175fed13b0711ff9d16
Ishankalhe911 <thegamechangerik@gmail.com>
2025-09-24 17:00:05 +0530

    delete script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const map = L.map('map').setView([23.6102, 85.2799], 7);
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-  }).addTo(map);
-});
